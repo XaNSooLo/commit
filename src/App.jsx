db8cfe2f4c3439f3a5012c787b6afaff964b203b
XaNSooLO <tolibovumar26@gmail.com>
2024-03-15 18:33:52 +0500

    fisrt commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import MainPage from "./Components/MainPage";
+
+function App() {
+  return (
+    <MainPage />
+  )
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,87 @@
+import Clear from "./img/icon/clear.png";
+import Clouds from "./img/icon/clouds.png";
+import Drizzle from "./img/icon/drizzle.png";
+import Humidity from "./img/icon/humidity.png";
+import Mist from "./img/icon/mist.png";
+import Rain from "./img/icon/rain.png";
+import Snow from "./img/icon/snow.png";
+import Wind from "./img/icon/wind.png";
+import Smoke from "./img/icon/smoke.png";
+import Storm from "./img/icon/storm.png";
+
+import DayClear from "./img/background/Webp/day-clear.jpg";
+import DayClouds from "./img/background/Webp/day-cloudy.jpg";
+import DayRain from "./img/background/Webp/day-rain.jpg";
+import DayDrizzle from "./img/background/Webp/day-drizzle.jpg";
+import DaySnow from "./img/background/Webp/day-snow.jpg";
+import DayHumidity from "./img/background/Webp/day-humidity.jpg";
+import DayWind from "./img/background/Webp/day-wind.jpg";
+import DaySmoke from "./img/background/Webp/day-smoke.jpg";
+import DayStorm from "./img/background/Webp/day-storm.jpg";
+
+import NightClear from "./img/background/Webp/night-clear.webp";
+import NightClouds from "./img/background/Webp/night-cloudy.webp";
+import NightDrizzle from "./img/background/Webp/night-drizzle.webp";
+import NightRain from "./img/background/Webp/night-rain.webp";
+import NightSnow from "./img/background/Webp/night-snow.webp";
+import NightHumidity from "./img/background/Webp/night-humidity.webp";
+import NightWind from "./img/background/Webp/night-wind.webp";
+import NightSmoke from "./img/background/Webp/night-smoke.webp";
+import NightStorm from "./img/background/Webp/night-storm.webp";
+
+export const icons = {
+  Clear,
+  Clouds,
+  Drizzle,
+  Humidity,
+  Mist,
+  Rain,
+  Snow,
+  Wind,
+  Smoke,
+  Storm,
+};
+
+export const bgImages = {
+  day: {
+    Clear: DayClear,
+    Clouds: DayClouds,
+    Drizzle: DayDrizzle,
+    Humidity: DayHumidity,
+    Rain: DayRain,
+    Mist: DaySmoke,
+    Snow: DaySnow,
+    Wind: DayWind,
+    Smoke: DaySmoke,
+    Haze: DaySmoke,
+    Storm: DayStorm,
+  },
+  night: {
+    Clear: NightClear,
+    Clouds: NightClouds,
+    Drizzle: NightDrizzle,
+    Humidity: NightHumidity,
+    Rain: NightRain,
+    Mist: NightSmoke,
+    Snow: NightSnow,
+    Wind: NightWind,
+    Smoke: NightSmoke,
+    Haze: NightSmoke,
+    Storm: NightStorm,
+  },
+};
+
+export const regions = [
+  "Andijan",
+  "Bukhara",
+  "Jizzakh",
+  "Navoi",
+  "Namangan",
+  "Qashqadaryo",
+  "Samarkand",
+  "Sirdaryo",
+  "Termez",
+  "Tashkent",
+  "Fergana",
+  "Khiva",
+];
